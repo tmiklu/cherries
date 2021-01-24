0985dd1aaf4536bc9f9642e9274ddc2e5ac3ef22
tommiklu <tomas.miklusicak@gmail.com>
2021-01-24 21:56:29 +0100

    Add game loop with event quit

--- main.py
+++ main.py
@@ -5,3 +5,14 @@ pygame.init()
 # create new window for graphics
 screen = pygame.display.set_mode((800, 600))
 
+#
+##
+### Game loop
+##
+#
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
