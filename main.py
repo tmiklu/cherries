2f4e5fbdcdda76a72ba4c4df247a072c7d3fe726
tommiklu <tomas.miklusicak@gmail.com>
2021-01-25 19:42:01 +0100

    Change distance to 22

--- main.py
+++ main.py
@@ -51,7 +51,8 @@ def cherry(x, y):
 
 def isCollision(bucket_x, bucket_y, cherry_x, cherry_y):
     distance = math.sqrt(math.pow(cherry_x - bucket_x, 2) + (math.pow(cherry_y - bucket_y, 2)))
-    if distance < 10:
+    # distance between cherry and bucket
+    if distance < 22:
         return True
     else:
         return False
